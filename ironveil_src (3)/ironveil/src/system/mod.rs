0c911f541c2afb314c3f999c12cafa58b25568e8
schizoidsaddam <billyjamesvarnell@gmail.com>
2026-06-10 00:02:42 -0400

    Delete ironveil_src (3)/ironveil directory

--- ironveil_src (3)/ironveil/src/system/mod.rs
+++
@@ -1,5 +0,0 @@
-pub mod docker;
-pub(crate) mod fingerprint;
-mod metrics;
-
-pub use metrics::{SystemMetrics, SystemPoller, WorldTension};
--- ironveil_src (3)/ironveil/src/world/mod.rs
+++
@@ -1,9 +0,0 @@
-pub mod gen;
-pub(crate) mod lore;
-mod names;
-mod simulation;
-pub(crate) mod state;
-mod tile;
-
-pub use simulation::WorldTick;
-pub use state::GameState;
